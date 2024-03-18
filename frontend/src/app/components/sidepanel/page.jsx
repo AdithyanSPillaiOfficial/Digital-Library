ae7d3732ae838743e6f51d2d51ac0b32b90fc660
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2024-03-19 00:59:27 +0530

    refactor: Added Side Panel to all pages

--- frontend/src/app/components/sidepanel/page.jsx
+++ frontend/src/app/components/sidepanel/page.jsx
@@ -1,22 +1,40 @@
 "use client";
-import React from 'react'
+import React, { useEffect } from 'react';
 import './page.css'
 import Image from 'next/image'
 import avatar from '/src/app/assets/avatar.jpg'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faLock } from '@fortawesome/free-solid-svg-icons'
+import { useRouter } from 'next/navigation';
+
+
+
 
 function sidePanel() {
+  const router = useRouter();
+  
+  useEffect(() => {
+    // Check if user is not logged in and redirect
+    if (!localStorage.isLogedIn) {
+      // router.push('/login');
+      window.location = '/login';
+    }
+  }, []); 
+
+
   return (
-    <div className='main'>
+    <div className='sidepanel-main'>
         <Image src={avatar} alt='Avatar' className='avatar'></Image>
-        <div className='icondiv'>
+        <div className='icondiv' onClick={()=>window.location='/'}>
             <FontAwesomeIcon icon={faHome} className='iconbtn' size='3x'/>
             <p className='btnlabel'>Home</p>
         </div>
 
-        <div className='icondiv logoutbtn'>
+        <div className='icondiv logoutbtn' onClick={()=>{
+          localStorage.removeItem('isLogedIn');
+          window.location = '/login';
+        }}>
             <FontAwesomeIcon icon={faLock} className='iconbtn' size='3x'/>
             <p className='btnlabel'>Log Out</p>
         </div>
--- frontend/src/app/search/page.jsx
+++ frontend/src/app/search/page.jsx
@@ -5,6 +5,7 @@ import './page.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import SidePanel from '../components/sidepanel/page';
 
 
 function Search() {
@@ -12,25 +13,27 @@ function Search() {
   var popularSearches = ['Data Structure', 'Java', 'DBMS', 'Flat', 'Compiler Design', 'LSD']  
   return (
     <div className='main'>
-        <h1>Discover New Books</h1>
-        <div className='search-div'>
-            <input type="search" className='input-search' placeholder='Search titles or authors' value={searchInput} onChange={(e)=>setSearchInput(e.target.value)}/>
-            <FontAwesomeIcon icon={faSearch} className='search-icon' size='2x'/>
-        </div>
-        <div className='horizontal-line' />
-        <h3>Popular searches</h3>
-        <div className='popular-searches'>
-            {
-                popularSearches.map((element, index) => (
-                    <div key={index} className='popular-searches-pebble' onClick={()=>{setSearchInput(element)}}>
-                        <p>{element}</p>
-                    </div>
-                ))
-            }
+        <SidePanel className='sidepanel'/>
+        <div className='page'>
+            <h1>Discover New Books</h1>
+            <div className='search-div'>
+                <input type="search" className='input-search' placeholder='Search titles or authors' value={searchInput} onChange={(e)=>setSearchInput(e.target.value)}/>
+                <FontAwesomeIcon icon={faSearch} className='search-icon' size='2x'/>
+            </div>
+            <div className='horizontal-line' />
+            <h3>Popular searches</h3>
+            <div className='popular-searches'>
+                {
+                    popularSearches.map((element, index) => (
+                        <div key={index} className='popular-searches-pebble' onClick={()=>{setSearchInput(element)}}>
+                            <p>{element}</p>
+                        </div>
+                    ))
+                }
             
+            </div>
+            <div className="horizontal-line" />
         </div>
-
-        <div className="horizontal-line" />
     </div>
   )
 }
