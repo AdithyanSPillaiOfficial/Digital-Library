7858ba33170e49f39680e71c84f8a037d7728075
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2024-03-27 16:03:37 +0530

    Added Book Design result in search

--- frontend/src/app/search/page.jsx
+++ frontend/src/app/search/page.jsx
@@ -6,36 +6,49 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import SidePanel from '../components/sidepanel/page';
+import Image from 'next/image';
 
+import bookthumb from '../assets/bookthumb.png'
 
 function Search() {
-  const [searchInput, setSearchInput] = useState('');
-  var popularSearches = ['Data Structure', 'Java', 'DBMS', 'Flat', 'Compiler Design', 'LSD']  
-  return (
-    <div className='main'>
-        <SidePanel className='sidepanel'/>
-        <div className='page'>
-            <h1>Discover New Books</h1>
-            <div className='search-div'>
-                <input type="search" className='input-search' placeholder='Search titles or authors' value={searchInput} onChange={(e)=>setSearchInput(e.target.value)}/>
-                <FontAwesomeIcon icon={faSearch} className='search-icon' size='2x'/>
-            </div>
-            <div className='horizontal-line' />
-            <h3>Popular searches</h3>
-            <div className='popular-searches'>
-                {
-                    popularSearches.map((element, index) => (
-                        <div key={index} className='popular-searches-pebble' onClick={()=>{setSearchInput(element)}}>
-                            <p>{element}</p>
+    const [searchInput, setSearchInput] = useState('');
+    var popularSearches = ['Data Structure', 'Java', 'DBMS', 'Flat', 'Compiler Design', 'LSD']
+    return (
+        <div className='main'>
+            <SidePanel className='sidepanel' />
+            <div className='page'>
+                <h1>Discover New Books</h1>
+                <div className='search-div'>
+                    <input type="search" className='input-search' placeholder='Search titles or authors' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+                    <FontAwesomeIcon icon={faSearch} className='search-icon' size='2x' />
+                </div>
+                <div className='horizontal-line' />
+                <h3>Popular searches</h3>
+                <div className='popular-searches'>
+                    {
+                        popularSearches.map((element, index) => (
+                            <div key={index} className='popular-searches-pebble' onClick={() => { setSearchInput(element) }}>
+                                <p>{element}</p>
+                            </div>
+                        ))
+                    }
+
+                </div>
+                <div className="horizontal-line" />
+                <div className='results'>
+                    <div className='book'>
+                        <Image src={bookthumb} alt='login image' className='thumbimage'></Image>
+                        <div>
+                            <h1 className='bookname'>Linear Algebra and Calculus</h1>
+                            <p>Author name : </p>
+                            <p>Department : </p>
+                            <p>SUbject : </p>
                         </div>
-                    ))
-                }
-            
+                    </div>
+                </div>
             </div>
-            <div className="horizontal-line" />
         </div>
-    </div>
-  )
+    )
 }
 
 export default Search
