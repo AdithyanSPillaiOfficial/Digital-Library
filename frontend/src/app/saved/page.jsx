ee38b829726d806dad8d59089da8dd7d1c6055ea
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2024-04-26 15:24:38 +0530

    fix : try 1

--- frontend/src/app/saved/page.jsx
+++ frontend/src/app/saved/page.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import SidePanel from '../components/sidepanel/page'
-import './page.css'
-import Book from '../components/book/book'
+// import React from 'react'
+// import SidePanel from '../components/sidepanel/page'
+// import './page.css'
+// import Book from '../components/book/book'
 
-const booktemp = {
-  name: 'Naughty Book'
-}
-function Saved() {
+// const booktemp = {
+//   name: 'Naughty Book'
+// }
+// function Saved() {
+//   return (
+//     <div className='main main-extra'>
+//         <SidePanel className='sidepanel'/>
+//         <div className='main-content'>
+//             <h1>Saved Books</h1>
+//             <div className='horizontal-line'></div>
+//             <div className='savedbooks'>
+//               <Book {...booktemp}/>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Saved
+
+import React from 'react'
+
+function page() {
   return (
-    <div className='main main-extra'>
-        <SidePanel className='sidepanel'/>
-        <div className='main-content'>
-            <h1>Saved Books</h1>
-            <div className='horizontal-line'></div>
-            <div className='savedbooks'>
-              <Book {...booktemp}/>
-            </div>
-        </div>
-    </div>
+    <div>page</div>
   )
 }
 
-export default Saved
+export default page
